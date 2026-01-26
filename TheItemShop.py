4a4e9d5a8c83e1d2654a4fd3763b4388c9c165ab
Predrag Komljenovic <pkomljenovic@students.mchenry.edu>
2026-01-26 14:47:27 -0600

    📚 Assignment 2B: Data Types

---
+++ TheItemShop.py
@@ -0,0 +1,33 @@
+"""
+📚 ADD-100: Intro to Python
+📝 Assignment: The RPG Character Profile
+"""
+
+# Defining variables of different data types
+character_name = "Mark"    # String
+level = 31                   # Integer
+health = 0               # Float
+is_alive = False               # Boolean
+if is_alive == False:
+    game_over = True
+else:
+    game_over = False
+# Printing all variables using f-strings
+print(f"Character Name: {character_name}")
+print(f"Level: {level}")
+print(f"Health: {health}")
+print(f"Alive: {is_alive}")
+print(f"Game Over: {game_over}")
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[] 1. Header Docstring included.
+[] 2. Define a String variable.
+[] 3. Define an Integer variable.
+[] 4. Define a Float variable.
+[] 5. Define a Boolean variable.
+[] 6. Print all variables using F-Strings.
+[] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
